9c3590a894fc9c60a94719e2ba7d486b3898f739
masterit92 <thebinh1992@gmail.com>
2013-11-01 23:56:13 +0700

    Binh

--- application/models/tblImages.php
+++ application/models/tblImages.php
@@ -1,8 +1,14 @@
 <?php
-class Models_tblImages extends  Libs_Model{
-   public function __construct(){
-       parent::__construct();
-   }
+
+class Models_tblImages extends Libs_Model {
+
+    private $queryUnit;
+
+    public function __construct() {
+        parent::__construct();
+        $this->queryUnit = new Libs_QueryUnit();
+    }
+
     private $img_id;
     private $pro_id;
     private $url;
@@ -10,52 +16,58 @@ class Models_tblImages extends  Libs_Model{
     /**
      * @param mixed $img_id
      */
-    public function setImgId($img_id)
-    {
+    public function setImgId($img_id) {
         $this->img_id = $img_id;
     }
 
     /**
      * @return mixed
      */
-    public function getImgId()
-    {
+    public function getImgId() {
         return $this->img_id;
     }
 
     /**
      * @param mixed $pro_id
      */
-    public function setProId($pro_id)
-    {
+    public function setProId($pro_id) {
         $this->pro_id = $pro_id;
     }
 
     /**
      * @return mixed
      */
-    public function getProId()
-    {
+    public function getProId() {
         return $this->pro_id;
     }
 
     /**
      * @param mixed $url
      */
-    public function setUrl($url)
-    {
+    public function setUrl($url) {
         $this->url = $url;
     }
 
     /**
      * @return mixed
      */
-    public function getUrl()
-    {
+    public function getUrl() {
         return $this->url;
     }
 
-    public function getAllImageByIDPro($pro_id){
-
+    public function setCustomerValue($row, $isKey = FALSE) {
+        
     }
+
+    public function getAllImageByIDPro($pro_id) {
+        $listImg = array();
+        $execute = $this->queryUnit->getSelect('tbl_images', " pro_id= '$pro_id'");
+        if (mysql_num_rows($execute) > 0) {
+            while ($row = mysql_fetch_assoc($execute)) {
+                
+            }
+        }
+        return $listImg;
+    }
+
 }
