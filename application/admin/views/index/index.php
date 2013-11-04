c5ef472954adf0ccc42a547bb8cdb7f1905d5867
Du Dinh Hieu <hieudd@smartosc.com>
2013-11-04 19:47:06 +0700

    Thái sửa template

--- application/admin/views/index/index.php
+++ application/admin/views/index/index.php
@@ -6,51 +6,69 @@ if (isset($_SESSION['user_admin']) && isset($_SESSION['full_name_admin'])) {
     <?php
 }
 ?>
+<style>
+    fieldset{
+        width: 500px;
+        height: 200px;
+        margin: 0 auto;
+        border: 1px black solid;
+        margin-top: 50px;
+        background: lavender;
+    }
+    legend{
+        width: 50px;
+        height: 20px;
+        border: 1px black solid;
+        background: yellowgreen;
+        font-weight: bold;
+        color: red;
+        margin-left: 20px;
+        padding-top: 10px;
+    }
+    form{
+        margin: 0 auto;
+        margin-top: 30px;
+    }
+    form label{
+        float: left;
+        width: 100px;
+        margin-left: 100px; 
+    }
+    form input{
+        border: 1px black solid;
+        margin-bottom: 10px;
+    }
+    h2{
+        margin-top: 100px;
+    }
+</style>
+<center>
+    <h3>Welcome to admin</h3>
+</center>
+<fieldset>
+    <legend>Login</legend>
+    <form method="post">
+        <center>
+            <div>
+                <label>User name:</label>
+                <input name="txtUser" id="username" type="text" value="" />
+            </div>
 
-<div class="container-fluid">
-    <div class="row-fluid">
-
-        <div class="row-fluid">
-            <div class="span12 center login-header">
-                <h2>Welcome to Charisma</h2>
-            </div><!--/span-->
-        </div><!--/row-->
-
-        <div class="row-fluid">
-            <div class="well span5 center login-box">
-                <div class="alert alert-info">
-                    Please login with your Username and Password.
+            <div>
+                <label>Pass word:</label>
+                <input class="input-large span10" name="txtPass" id="password" type="password" value="" />
+            </div>
+            <?php if (isset($_SESSION['confirm']) && $_SESSION['confirm'] >= 4) { ?>
+                <div>
+                    <label>
+                        <img src="<?php echo URL_BASE ?>/templates/random_image.php" width="100" height="40" alt="No image" title="Confirmation code"/>
+                    </label>
+                    <input type="text" name="confirm" id="confirm"/>
                 </div>
-                <form class="form-horizontal" method="post">
-                    <fieldset>
-                        <div class="input-prepend" title="Username" data-rel="tooltip">
-                            <span class="add-on"><i class="icon-user"></i></span><input autofocus class="input-large span10" name="txtUser" id="username" type="text" value="" />
-                        </div>
-                        <div class="clearfix"></div>
-
-                        <div class="input-prepend" title="Password" data-rel="tooltip">
-                            <span class="add-on"><i class="icon-lock"></i></span><input class="input-large span10" name="txtPass" id="password" type="password" value="" />
-                        </div>
-                        <?php if (isset($_SESSION['confirm']) && $_SESSION['confirm'] >= 4) { ?>
-                            <div class="confirm">
-                                <span class="img_confirm"><img src="<?php echo URL_BASE ?>/templates/random_image.php" width="100" height="40" alt="No image" title="Confirmation code"/></span>
-                                <input type="text" name="confirm" id="confirm" class="txt txtConfirm required" tabindex="3" placeholder="Confirm"/>
-                            </div>
-                        <?php } ?>
-                        <div class="clearfix"></div>
-
-                        <div class="input-prepend">
-                            <label class="remember" for="remember"><input type="checkbox" id="remember" />Remember me</label>
-                        </div>
-                        <div class="clearfix"></div>
-
-                        <p class="center span5">
-                            <input type="submit" value="Login" name="btnLogin" id="tbnLogin" class="btn btn-primary" tabindex="5"/>
-                        </p>
-                    </fieldset>
-                </form>
-            </div><!--/span-->
-        </div><!--/row-->
-    </div><!--/fluid-row-->
-
-</div><!--/.fluid-container-->
+            <?php } ?>
+            <p>
+                <input type="submit" value="Login" name="btnLogin" id="tbnLogin"/>
+            </p>
+        </center>
+    </form>
+</fieldset>
