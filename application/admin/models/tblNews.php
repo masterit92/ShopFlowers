e90bb844d60986ee0673c5f0dc5080571cf2eed0
ThaiNV <thainv@smartosc.com>
2013-11-04 13:37:35 +0700

    Thái comit file logic tblNews

--- application/admin/models/tblNews.php
+++ application/admin/models/tblNews.php
@@ -1,12 +1,48 @@
 <?php
-class Admin_Models_tblNews extends  Models_tblNews{
-    public function insertNew(Admin_Models_tblNews $new){
 
-    }
-    public function updateNew(Admin_Models_tblNews $new){
+/**
+ * @desc: logic of News
+ * 
+ * @author: ThaiNV
+ * @since: 04-11-2013
+ */
+class Admin_Models_tblNews extends Models_tblNews {
 
-    }
-    public function deleteNew($new_id){
+    protected $_queryUnit;
 
+    public function __construct() {
+        parent::__construct();
+        $this->_queryUnit = new Libs_QueryUnit();
     }
+
+    /**
+     * @desc: get list data of news
+     * 
+     * @author: ThaiNV
+     * @since: 04-11-2013
+     */
+    public function getListNews(&$aryResult, $aryCondition) {
+        try {
+            //Build data to get list
+            $aryData = array(
+                'news_id',
+                'news_title',
+                'news_content',
+                'news_start_date',
+                'news_end_date',
+                'news_image'
+            );
+            $orderBy = 'news_id DESC';
+            //Get query
+            $query = $this->_queryUnit->getSelect('tbl_news', null, $orderBy);
+            //Get result
+            $aryResult = mysql_fetch_assoc($query);
+            $intIsOk = 1;
+        } catch (Exception $e) {
+            echo $e->getMessage();
+            $intIsOk = -1;
+        }
+        return $intIsOk;
+    }
+
 }
