8972e73eaf81353fb62a214a05d5e6cfda6a87ca
ThaiNV <thainv@smartosc.com>
2013-11-07 03:25:33 +0700

    recomit

--- application/admin/controllers/news.php
+++ application/admin/controllers/news.php
@@ -27,7 +27,7 @@ class Admin_Controllers_News extends Libs_Controller {
      */
     public function listNews() {
         $aryResult = $aryCodition = array();
-        $oldKeyWord = $oldPubKeyWord = '';
+        $oldKeyWord = '';
         $keyword = $_POST['txtKey_word'];
         if (!empty($keyword)) {
             $oldKeyWord = $keyword;
