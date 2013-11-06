380390d7debd8a555214781e81be95002b525fab
ThaiNV <thainv@smartosc.com>
2013-11-07 00:46:34 +0700

    Thái commit views/news

---
+++ application/admin/views/news/formData.php
@@ -0,0 +1,59 @@
+<script type="text/javascript" src="<?php echo URL_BASE . '/templates/admin/style/ckeditor/ckeditor.js' ?>"></script>
+<script type="text/javascript" src="<?php echo URL_BASE . '/templates/admin/style/js/news/NewsController.js' ?>"></script>
+<h3>Another section</h3>
+<fieldset>
+    <form name="frmData" method="post" action="<?php echo URL_BASE . '/admin/news/save' ?>" onsubmit=" return NewsController.validateFormData();">
+        <?php
+        if ($this->aryData == -1) {//------------ADD------------
+            ?>
+            <p>
+                <label>Ngày đăng:</label>
+                <input type="text" class="datepicker" class="text-medium" name="txtDate_start" id="date-start" />
+            </p>
+            <p>
+                <label>Ngày hết hạn:</label>
+                <input type="text" class="datepicker" class="text-medium" name="txtDate_end" id="date-end" />
+            </p>
+            <p>
+                <label>Tiêu đề:</label>
+                <input type="text" class="text-long" name="txtNews_title" id="news-title" />
+            </p>
+            <p>
+                <label>Nội dung:</label>
+                <textarea rows="1" cols="1" name="txtNews_content" id="news-content" ></textarea>
+                <script type="text/javascript">CKEDITOR.replace('news-content');</script>
+            </p>
+            <?php
+        } else {
+            foreach ($this->aryData as $value) {//------------EDIT------------
+                ?>
+                <input type="hidden" name="news_id" value="<?php echo $value['news_id'] ?>">
+                <p>
+                    <label>Ngày đăng:</label>
+                    <input type="text" class="datepicker" class="text-medium" name="txtDate_start" id="date-start" value="<?php echo $value['news_start_date'] ?>"/>
+                </p>
+                <p>
+                    <label>Ngày hết hạn:</label>
+                    <input type="text" class="datepicker" class="text-medium" name="txtDate_end" id="date-end" value="<?php echo $value['news_end_date'] ?>"/>
+                </p>
+                <p>
+                    <label>Tiêu đề:</label>
+                    <input type="text" class="text-long" name="txtNews_title" id="news-title" value="<?php echo $value['news_title'] ?>"/>
+                </p>
+                <p>
+                    <label>Nội dung:</label>
+                    <textarea rows="1" cols="1" name="txtNews_content" id="news-content" ><?php echo $value['news_content'] ?></textarea>
+                    <script type="text/javascript">CKEDITOR.replace('news-content');</script>
+                </p>
+                <?php
+            }
+        }
+        ?>
+        <input type="submit" value="Submit Query" name="Ok"/>
+        <input type="reset" value="Reset"/>
+        <input type="button" value="Back" onclick="window.location.href = '<?php echo URL_BASE . '/admin/news' ?>';"/>
+    </form>
+</fieldset>
+
+
+
---
+++ application/admin/views/news/listNews.php
@@ -0,0 +1,63 @@
+
+<h3>Quản lý tin tức</h3>
+
+<form method="post" name="frmList_news">
+    <table cellpadding="0" cellspacing="0" border="1">
+        <thead>
+            <tr>
+                <td colspan="8">
+                    <a href="<?php echo URL_BASE . '/admin/news/loadFormData&id=0' ?>" class="add">Add new</a>
+                    <a href="#" onclick="BaseController.deleteMultiRecord('news', 'delete');" class="delete">Delete multi</a>
+                    <span>
+                        <input type="text" name="txtKey_word" id="txtKey_word" value="<?php echo $this->keyword ?>"/>
+                        <input type="submit" value="Search" />
+                        <input type="button" value="Reset" onclick="document.getElementById('txtKey_word').value = null;"/>
+                    </span>
+                </td>
+
+            </tr>
+            <tr>
+                <th><input type="checkbox" name="checkAll" id="checkAll" onclick="BaseController.checkAll();"></th>
+                <th>Stt</th>
+                <th>Tiêu đề</th>
+                <th>Nội dung</th>
+                <th>Ngày đăng</th>
+                <th>Ngày hết hạn</th>
+                <th>Ảnh</th>
+                <th>Tùy chọn</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $nbr = 0;
+            if ($this->aryData !== -1) {
+                foreach ($this->aryData as $value):
+                    $nbr++;
+                    ?>
+                    <tr>
+                        <td><input type="checkbox" name="checkOne[]" value="<?php echo $value['news_id'] ?>"></td>
+                        <td><?php echo $nbr ?></td>
+                        <td><?php echo $value['news_title'] ?></td>
+                        <td><?php echo $value['news_content'] ?></td>
+                        <td><?php echo $value['news_start_date'] ?></td>
+                        <td><?php echo $value['news_end_date'] ?></td>
+                        <td><?php echo $value['news_image'] ?></td>
+                        <td class="action">
+                            <a href="#" class="view">View</a>
+                            <a href="<?php echo URL_BASE . '/admin/news/loadFormData&id=' . $value['news_id'] ?>" class="edit">Edit</a>
+                            <a href="#" onclick="BaseController.deleteOneRecord('this new', '<?php echo URL_BASE . '/admin/news/delete&listId=' . $value['news_id'] ?>');" class="delete">Delete</a>
+                        </td>
+                    </tr>
+                    <?php
+                endforeach;
+            }else {
+                ?>
+                <tr>
+                    <td colspan="8" style="text-align: center; height: 50px;"><b>Không có dữ liệu</b></td>
+                </tr>
+            <?php } ?>
+        </tbody>
+
+    </table>
+    <?php echo $this->paging ?>
+</form>
