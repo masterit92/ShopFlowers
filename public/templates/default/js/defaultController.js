e9357837428a71a2e91c81586c850c3b44b8da3c
ThaiNV <thainv@smartosc.com>
2013-11-11 06:14:53 +0700

    Thái sửa để lưu đơn hàng

--- public/templates/default/js/defaultController.js
+++ public/templates/default/js/defaultController.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+    $('#areaDelivery').hide();
 });
 
 /**
@@ -24,8 +24,7 @@ var DefaultController = {
             url: url,
             data: 'proId=' + data,
             success: function(data) {
-                var obj = $.parseJSON(data);
-                $(".left_content").html(obj.cart);
+                DefaultController.showCart();
             }
         });
     },
@@ -43,6 +42,61 @@ var DefaultController = {
             success: function(data) {
                 var obj = $.parseJSON(data);
                 $(".left_content").html(obj.cart);
+            }
+        });
+    },
+    /**
+     * @description delete products from cart
+     *
+     * @author ThaiNV 
+     * @since 09/11/2013
+     */
+    deleteCart: function(proId) {
+        var url = BASE_URL + '/ShoppingCart/deleteCart';
+        var data = proId;
+        $.ajax({
+            type: "post",
+            url: url,
+            data: "proId=" + data,
+            success: function(data) {
+                DefaultController.showCart();
+            }
+        });
+    },
+    /**
+     * @description load form customer
+     *
+     * @author ThaiNV 
+     * @since 09/11/2013
+     */
+    loadFormCustom: function() {
+        var url = BASE_URL + '/shoppingCart/loadFormInfo';
+        var data = $('#frmCart').serialize();
+        $.ajax({
+            type: "post",
+            url: url,
+            data: data,
+            success: function(data) {
+                var obj = $.parseJSON(data);
+                $(".left_content").html(obj.form);
+            }
+        });
+    },
+    /**
+     * @description save customer information
+     *
+     * @author ThaiNV 
+     * @since 09/11/2013
+     */
+    saveOrder: function() {
+        var url = BASE_URL + '/shoppingCart/saveOrderInfo';
+        var data = $('#frm-Customer').serialize();
+        $.ajax({
+            type: "post",
+            url: url,
+            data: data,
+            success: function(data) {
+
             }
         });
     }
