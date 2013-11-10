9dd7bc9a958a44670cd4d80a1b12e34b16862b34
ThaiNV <thainv@smartosc.com>
2013-11-11 06:13:53 +0700

    Thái sửa lưu thông tin đơn hàng

--- application/default/models/tblCustomers.php
+++ application/default/models/tblCustomers.php
@@ -1,35 +1,78 @@
 <?php
-class Default_Models_tblCustomers extends Models_tblCustomers{
+
+class Default_Models_tblCustomers extends Models_tblCustomers {
 
     public function __construct() {
         parent::__construct();
-        $this->queryUnit= new Libs_QueryUnit();
+        $this->queryUnit = new Libs_QueryUnit();
     }
+
     private $queryUnit;
 
-    public function insertCustomer(Default_Models_tblCustomers $cus){
-        $execute= $this->queryUnit->getInsert('tbl_customers', $this->getColumnAndValue($cus));
-        if($execute){
+    public function insertCustomer(Default_Models_tblCustomers $cus) {
+        $execute = $this->queryUnit->getInsert('tbl_customers', $this->getColumnAndValue($cus));
+        if ($execute) {
             return TRUE;
         }
         return FALSE;
     }
-    public function updateCustomer(Default_Models_tblCustomers $cus){
-        $execute= $this->queryUnit->getUpdate('tbl_customers', $this->getColumnAndValue($cus, True), "cus_id='".$cus->getCusId()."'");
-        if($execute){
+
+    public function updateCustomer(Default_Models_tblCustomers $cus) {
+        $execute = $this->queryUnit->getUpdate('tbl_customers', $this->getColumnAndValue($cus, True), "cus_id='" . $cus->getCusId() . "'");
+        if ($execute) {
             return TRUE;
         }
         return FALSE;
     }
-    public function checkLogin($email, $pass){
-        $cus= null;
-        if(!empty($email) && !empty($pass)){
-            $execute= $this->queryUnit->getSelect('tbl_customers', "email='$email' and password='$pass'");
-            if(mysql_num_rows($execute)>0){
-                $row= mysql_fetch_assoc($execute);
+
+    public function checkLogin($email, $pass) {
+        $cus = null;
+        if (!empty($email) && !empty($pass)) {
+            $execute = $this->queryUnit->getSelect('tbl_customers', "email='$email' and password='$pass'");
+            if (mysql_num_rows($execute) > 0) {
+                $row = mysql_fetch_assoc($execute);
                 $this->setCustomerValue($row);
             }
         }
         return $cus;
     }
+
+    /**
+     * @description save custom info
+     * 
+     * @author ThaiNV
+     * @since 11/11/2013
+     */
+    public function saveCustomer($aryParams) {
+        try {
+            $aryDataInsert = array(
+                'first_name' => $aryParams['txtCus_firtName'],
+                'last_name' => $aryParams['txtCus_lastName'],
+                'email' => $aryParams['txtCus_email'],
+                'phone' => $aryParams['txtCus_phone'],
+                'address' => $aryParams['txtCus_adrres'],
+            );
+            $this->queryUnit->getInsert('tbl_customers', $aryDataInsert);
+            $intIsOk = 1;
+        } catch (Exception $exc) {
+            echo $exc->getTraceAsString();
+            $intIsOk = -1;
+        }
+        return $intIsOk;
+    }
+
+    /**
+     * @description build data to save order info
+     * 
+     * @author ThaiNV
+     * @since 11/11/2013
+     */
+    public function buildDataOrder($aryParams) {
+        $this->saveCustomer($aryParams);
+        $aryReceived['name'] = ($aryParams['txtRec_name	'] == '') ? $aryParams['txtCus_lastName	'] . $aryParams['txtCus_firtName'] : $aryParams['txtRec_name'];
+        $aryReceived['email'] = ($aryParams['txtRec_email'] == '') ? $aryParams['txtCus_email'] : $aryParams['txtRec_email'];
+        $aryReceived['adress'] = ($aryParams['txtRec_adress'] == '') ? $aryParams['txtCus_adrres'] : $aryParams['txtRec_adress'];
+        
+    }
+
 }
