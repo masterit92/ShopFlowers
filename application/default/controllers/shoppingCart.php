8c506fe12249739b7f9844ce86bfbe6eb17ab814
ThaiNV <thainv@smartosc.com>
2013-11-12 06:57:45 +0700

    TháiNV sửa shoppingCart.php

--- application/default/controllers/shoppingCart.php
+++ application/default/controllers/shoppingCart.php
@@ -17,26 +17,25 @@ class Default_Controllers_ShoppingCart extends Libs_Controller {
         $this->_logic = new Default_Models_tblProducts();
     }
 
+    public function index() {
+        
+    }
+
     /**
      * @description update qty of products in cart
      *
      * @author ThaiNV 
      * @since 09/11/2013
      */
-    public function index() {
-        if (isset($_POST['Ok'])) {
-            foreach ($_POST['qty'] as $key => $value) {
-                if (($value == 0) and (is_numeric($value))) {
-                    unset($_SESSION['cart'][$key]);
-                } elseif (($value > 0) and (is_numeric($value))) {
-                    $_SESSION['cart'][$key] = $value;
-                    $_SESSION['totalMoney'] = $this->renderViewCart();
-                }
+    public function updateCart() {
+        $aryQty = $_POST['qty'];
+        foreach ($aryQty as $key => $value) {
+            if (($value == 0) and (is_numeric($value))) {
+                unset($_SESSION['cart'][$key]);
+            } elseif (($value > 0) and (is_numeric($value))) {
+                $_SESSION['cart'][$key] = $value;
             }
         }
-        $this->view->render('shoppingCart/index');
-        $loadJS = $this->loadJsFunction('DefaultController.showCart()');
-        echo $loadJS;
     }
 
     /**
@@ -116,17 +115,17 @@ class Default_Controllers_ShoppingCart extends Libs_Controller {
                 $view .= '<td><a href=""><img src="images/cart_thumb.gif" alt="" border="0" class="cart_thumb" /></a></td>';
                 $view .= '<td>' . $value['name'] . '</td>';
                 $view .= '<td>' . $value['price'] . ' $' . '</td>';
-                $view .= '<td>' . "<input style='text-align: center; border: 1px black solid' type='text' size='5' maxlength='10'" . 'value=' . $_SESSION['cart'][$value['pro_id']] . " name='qty[" . $value['pro_id'] . "]'></input>" . '</td>';
+                $view .= '<td>' . "<input style='text-align: center; border: 1px black solid' type='text' size='5' maxlength='10'" . 'value=' . $_SESSION['cart'][$value['pro_id']] . " name='qty[" . $value['pro_id'] . "]' onchange='DefaultController.updateCart()'></input>" . '</td>';
                 $view .= '<td>' . $value['description'] . '</td>';
                 $view .= '<td>' . $_SESSION['cart'][$value['pro_id']] * $value['price'] . ' $' . '</td>';
                 $view .= '<td><input type="button" onclick="DefaultController.deleteCart(' . $value["pro_id"] . ')" value="Delete"></td>';
-                $view .= '<td><input type="hidden" name="product[' . $value["name"] . ']" value="' . $value["pro_id"] . '"></td>';
+                $view .= '<td><input type="hidden" name="id[' . $value["name"] . ']" value="' . $value["pro_id"] . '"></td>';
+                $view .= '<td><input type="hidden" name="price[' . $value["name"] . ']" value="' . $value["price"] . '"></td>';
                 $view .= '</tr>';
                 $total += $_SESSION['cart'][$value['pro_id']] * $value['price'];
             }
             $view .= '<tr>';
-            $view .= '<td colspan="1"><input style=" color: white ; background: #8A181A; border: 1px #8A181A solid;border-radius: 5px;" type = "submit" value = "Save" name = "Ok"/></td>';
-            $view .= '<td colspan="3"></td>';
+            $view .= '<td colspan="4"></td>';
             $view .= ' <td colspan="1" class="cart_total"><span class="red">TOTAL: </span></td>';
             $view .= '<td>' . $total . ' $' . '</td>';
             $view .= '<td></td>';
@@ -134,10 +133,6 @@ class Default_Controllers_ShoppingCart extends Libs_Controller {
         } else {
             $view .= '<tr>';
             $view .= '<td colspan="7"> Your cart is empty</td>';
-            $view .= '</tr>';
-            $view .= '<tr>';
-            $view .= '<td colspan="1"><input style=" color: white ; background: #8A181A; border: 1px #8A181A solid;border-radius: 5px;" type = "submit" value = "Save" name = "Ok"/></td>';
-            $view .= '<td colspan="6"></td>';
             $view .= '</tr>';
         }
 
@@ -162,7 +157,6 @@ class Default_Controllers_ShoppingCart extends Libs_Controller {
             unset($_SESSION['cart']);
         } else {
             unset($_SESSION['cart'][$proId]);
-            $_SESSION['totalMoney'] = $this->renderViewCart();
         }
     }
 
@@ -173,37 +167,84 @@ class Default_Controllers_ShoppingCart extends Libs_Controller {
      * @since 09/11/2013
      */
     public function showCart() {
-        $cart = '';
-        $_SESSION['totalMoney'] = $this->renderViewCart($cart);
-        echo json_encode(array('cart' => $cart));
+        $cart = $infoCart = '';
+        $numItems = (count($_SESSION['cart']) == 0 ) ? 0 : count($_SESSION['cart']);
+        $totalMoney = $this->renderViewCart($cart);
+        $infoCart = $numItems . ' x items | <span class="red">TOTAL: ' . $totalMoney . ' $' . '</span>';
+        echo json_encode(array('cart' => $cart, 'cartInfo' => $infoCart));
         exit();
     }
 
     /**
-     * @description load javascript function
+     * @description get cart information
      * 
      * @author ThaiNV 
      * @since 09/11/2013
      */
-    public function loadJsFunction($jsFuncName) {
-        $script = '<script type="text/javascript" src="' . URL_BASE . '/templates/default/js/jquery-1.10.2.js"></script>';
-        $script .= '<script type="text/javascript" src="' . URL_BASE . '/templates/default/js/defaultController.js"></script>';
-        $script .= '<script type="text/javascript"> var BASE_URL = ' . '"' . URL_BASE . '"' . '</script>';
-        $script .= '<script type="text/javascript">' . $jsFuncName . '</script>';
-        return $script;
+    public function getCartInfo() {
+        $numItems = (count($_SESSION['cart']) == 0 ) ? 0 : count($_SESSION['cart']);
+        $totalMoney = $this->renderViewCart();
+        $infoCart = $numItems . ' x items | <span class="red">TOTAL: ' . $totalMoney . ' $' . '</span>';
+        echo json_encode(array('cartInfo' => $infoCart));
+        exit();
     }
 
     /**
-     * @description load form customer
+     * @description load form customer information
      * 
      * @author ThaiNV 
      * @since 09/11/2013
      */
     public function loadFormInfo() {
+        $aryProId = $_POST['id'];
+        $aryQty = $_POST['qty'];
+        $aryPrice = $_POST['price'];
         $form = file_get_contents('http://' . $_SERVER['SERVER_NAME'] . '/shopFlowers/application/default/views/shoppingCart/formInfo.php');
 //        $strId = join(',', $_POST['product']);
         echo json_encode(array('form' => $form));
         exit();
+    }
+
+    /**
+     * @description load form customer information
+     * 
+     * @author ThaiNV 
+     * @since 09/11/2013
+     */
+    public function loadPaymentMethod() {
+        $aryParams = $_POST;
+        $payView = $this->renderPayMethod();
+        echo json_encode(array('view' => $payView));
+        exit();
+    }
+
+    /**
+     * @description render view of Payment Method
+     * 
+     * @author ThaiNV 
+     * @since 09/11/2013
+     */
+    public function renderPayMethod() {
+        $aryPayment = $this->getPayMethod();
+        $payView = "<h1>Phuong thuc thanh toan</h1>";
+        foreach ($aryPayment as $value) {
+            $payView .= "<h3><input type='radio' name='methodName' id='" . $value['pay_id'] . "'>" . $value['name'] . "</h3>";
+            $payView .= "<p>" . $value['content'] . "</p>";
+            $payView .= "<p>" . $value['image'] . "</p>";
+        }
+        return $payView;
+    }
+
+    /**
+     * @description get list payment method
+     * 
+     * @author ThaiNV 
+     * @since 09/11/2013
+     */
+    public function getPayMethod() {
+        $payMethod = new Default_Models_tblPaymentMethod();
+        $aryPayment = $payMethod->getPayment();
+        return $aryPayment;
     }
 
     /**
