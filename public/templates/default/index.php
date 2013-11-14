d0f7fd6f8ba56394e7eb1e59307818f04b1cb0c5
ThaiNV <thainv@smartosc.com>
2013-11-15 05:05:39 +0700

    ThaiNV comit cart

--- public/templates/default/index.php
+++ public/templates/default/index.php
@@ -9,7 +9,7 @@
         <?php
         include_once 'templates/render.php';
         $render = new render();
-        $arrFileCSS = array('style.css', 'style_menuLogin.css', 'content_css.css');
+        $arrFileCSS = array('style.css', 'style_menuLogin.css', 'content_css.css', 'jquery-ui-1.10.3.custom.css');
         $render->renderCSS('default', $arrFileCSS);
         $arrFileJS = array(
             'jquery-1.10.2.js',
@@ -18,13 +18,19 @@
             'js_index.js',
             'defaultController.js',
             'jquery-1.8.3.min.js',
-            'jquery.elevatezoom.js'
+            'jquery.elevatezoom.js',
+            'jquery-ui-1.10.3.custom.js'
         );
         $render->renderJS('default', $arrFileJS);
         ?>
         <link href="<?php echo URL_BASE ?>/favicon.ico" rel="shortcut icon" />
-        <!--ThaiNV: Set root url-->
+
         <script type="text/javascript">
+            //ThaiNV: set datepicker for all project:
+            $(window).load(function() {
+                $(".datepicker").datepicker();
+            });
+            //ThaiNV: Set root url
             var BASE_URL = '<?PHP echo URL_BASE ?>';
         </script>
 
@@ -45,15 +51,15 @@
                     var url = '<?php echo URL_BASE . '/products?order=DESC'; ?>';
                     $("#content_left").load(url);
                 });
-                $(".cat").click(function (){
-                    var catID= $(this).attr('cat_id');
-                    var url = '<?php echo URL_BASE . '/products?cat_id='; ?>'+catID;
+                $(".cat").click(function() {
+                    var catID = $(this).attr('cat_id');
+                    var url = '<?php echo URL_BASE . '/products?cat_id='; ?>' + catID;
                     $("#content_left").load(url);
                 });
-                
+
                 $(".zoom_01").elevateZoom({zoomWindowPosition: 1, zoomWindowOffetx: 10});
-                
-                
+
+
             });
         </script>
     </head>
@@ -100,86 +106,86 @@
                             <b>Price:</b> 
                             <br/><input type="radio" name="rdb" id="priceMin">Min >> Max
                                 <br/><input type="radio" name="rdb" id="priceMax">Max >> Min
-                        </div>
-                    </div>
-                    <div class="right_content_half">
-                        <div class="title">
-                            <span class="title_icon"><img src="<?php echo URL_BASE ?>/templates/default/images/bullet4.gif" alt=""/></span>
-                            Best Sellers
-                        </div>
-                        <div class="clear"></div>
-                        <div class="right_content_half_content">
-                            Best Sellers<br/>
-                            Best Sellers<br/>
-                            Best Sellers<br/>
-                            Best Sellers<br/>
-                            Best Sellers<br/>
-                            Best Sellers<br/>
-                            Best Sellers<br/>
-                            Best Sellers<br/>
-                        </div>
-                    </div>
-                    <div class="right_content_half">
-                        <div class="title">
-                            <span class="title_icon"><img src="<?php echo URL_BASE ?>/templates/default/images/bullet5.gif" alt=""/></span>
-                            Promotions
-                        </div>
-                        <div class="clear"></div>
-                        <div class="right_content_half_box">
-                            Promotions
-                        </div>
-                    </div>
-                    <div class="right_content_half">
-                        <div class="title">
-                            <span class="title_icon"><img src="<?php echo URL_BASE ?>/templates/default/images/bullet6.gif" alt=""/></span>
-                            Category
-                        </div>
-                        <div class="clear"></div>
-                        <div class="right_content_half_box">
-                            <?php
-                            include_once 'listCategory.php';
-                            ?>  
-                        </div>
-                    </div>
+                                    </div>
+                                    </div>
+                                    <div class="right_content_half">
+                                        <div class="title">
+                                            <span class="title_icon"><img src="<?php echo URL_BASE ?>/templates/default/images/bullet4.gif" alt=""/></span>
+                                            Best Sellers
+                                        </div>
+                                        <div class="clear"></div>
+                                        <div class="right_content_half_content">
+                                            Best Sellers<br/>
+                                            Best Sellers<br/>
+                                            Best Sellers<br/>
+                                            Best Sellers<br/>
+                                            Best Sellers<br/>
+                                            Best Sellers<br/>
+                                            Best Sellers<br/>
+                                            Best Sellers<br/>
+                                        </div>
+                                    </div>
+                                    <div class="right_content_half">
+                                        <div class="title">
+                                            <span class="title_icon"><img src="<?php echo URL_BASE ?>/templates/default/images/bullet5.gif" alt=""/></span>
+                                            Promotions
+                                        </div>
+                                        <div class="clear"></div>
+                                        <div class="right_content_half_box">
+                                            Promotions
+                                        </div>
+                                    </div>
+                                    <div class="right_content_half">
+                                        <div class="title">
+                                            <span class="title_icon"><img src="<?php echo URL_BASE ?>/templates/default/images/bullet6.gif" alt=""/></span>
+                                            Category
+                                        </div>
+                                        <div class="clear"></div>
+                                        <div class="right_content_half_box">
+                                            <?php
+                                            include_once 'listCategory.php';
+                                            ?>  
+                                        </div>
+                                    </div>
 
-                    <div class="clear"></div>
-                    <div>
-                        <div class="title">
-                            <span class="title_icon"><img src="<?php echo URL_BASE ?>/templates/default/images/news.png" alt=""/></span>
-                            News
-                        </div>
-                        <div class="clear"></div>
-                        <div class="right_content_half_box">
-                            Hot new
-                        </div>
-                    </div>
-                    <div class="">
-                        Quảng cáo ở đây
-                        <?php
-                            include_once 'listAds.php';
-                        ?>
-                    </div>
-                    <!--end of Search-->
-                    <!--end of cart-->
-                </div>
-                <!--end of right content-->
-                <div class="clear"></div>
-                <div class=""></div>
-            </div>
-            <!--end of center content-->
-            <div class="footer">
-                <div class="left_footer"><img src="<?php echo URL_BASE ?>/templates/default/images/footer_logo.gif" alt=""/></div>
-                <div class="right_footer">
-                    <a href="<?php echo URL_BASE ?>/index">home</a>
-                    <a href="<?php echo URL_BASE ?>/about">about us</a>
-                    <a href="#">services</a>
-                    <a href="#">contact us</a>
-                </div>
-                <div class="clear"></div>
-                <div class="copy_right">&#169;Copyright, Development by trainee SmartOSC&#174; </div>
-            </div>
-            <!--end of footer-->
-        </div>
-        <!--end of warp-->
-    </body>
-</html>
+                                    <div class="clear"></div>
+                                    <div>
+                                        <div class="title">
+                                            <span class="title_icon"><img src="<?php echo URL_BASE ?>/templates/default/images/news.png" alt=""/></span>
+                                            News
+                                        </div>
+                                        <div class="clear"></div>
+                                        <div class="right_content_half_box">
+                                            Hot new
+                                        </div>
+                                    </div>
+                                    <div class="">
+                                        Quảng cáo ở đây
+                                        <?php
+                                        include_once 'listAds.php';
+                                        ?>
+                                    </div>
+                                    <!--end of Search-->
+                                    <!--end of cart-->
+                                    </div>
+                                    <!--end of right content-->
+                                    <div class="clear"></div>
+                                    <div class=""></div>
+                                    </div>
+                                    <!--end of center content-->
+                                    <div class="footer">
+                                        <div class="left_footer"><img src="<?php echo URL_BASE ?>/templates/default/images/footer_logo.gif" alt=""/></div>
+                                        <div class="right_footer">
+                                            <a href="<?php echo URL_BASE ?>/index">home</a>
+                                            <a href="<?php echo URL_BASE ?>/about">about us</a>
+                                            <a href="#">services</a>
+                                            <a href="#">contact us</a>
+                                        </div>
+                                        <div class="clear"></div>
+                                        <div class="copy_right">&#169;Copyright, Development by trainee SmartOSC&#174; </div>
+                                    </div>
+                                    <!--end of footer-->
+                                    </div>
+                                    <!--end of warp-->
+                                    </body>
+                                    </html>
