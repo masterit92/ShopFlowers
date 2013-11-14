a1ac8f7243b58994b9b96ea816ffe2fd00f6d331
ThaiNV <thainv@smartosc.com>
2013-11-15 05:05:03 +0700

    ThaiNV comit cart

--- application/default/views/shoppingCart/formInfo.php
+++ application/default/views/shoppingCart/formInfo.php
@@ -6,6 +6,7 @@
     $("#delivery").click(function() {
         $("#areaDelivery").show();
     });
+    $(".datepicker").datepicker();
 </script>
 <div class="title"><span class="title_icon"><img src="images/bullet1.gif" alt="" /></span>Register</div>
 <div class="feat_prod_box_details">
@@ -14,25 +15,30 @@
         <div class="form_subtitle">Customers infomation</div>
         <form name="frmCustomer" id="frm-Customer">
             <p style="border-bottom: 1px black dashed; width: 100%; text-align: center"><span style="color: #A81F22;">Payment adrres</span></p>
-            <div class="form_row">
-                <label class="contact"><strong>First name:</strong></label>
-                <input type="text" class="contact_input" name="txtCus_firtName"/>
+            <div class="customerInformation">
+
             </div>
-            <div class="form_row">
-                <label class="contact"><strong>Last name:</strong></label>
-                <input type="text" class="contact_input" name="txtCus_lastName"/>
-            </div>
-            <div class="form_row">
-                <label class="contact"><strong>Email:</strong></label>
-                <input type="text" class="contact_input" name="txtCus_email"/>
-            </div>
-            <div class="form_row">
-                <label class="contact"><strong>Phone:</strong></label>
-                <input type="text" class="contact_input" name="txtCus_phone"/>
-            </div>
-            <div class="form_row">
-                <label class="contact"><strong>Adrres:</strong></label>
-                <input type="text" class="contact_input" name="txtCus_adrres"/>
+            <div class="guestInfomartion">
+                <div class="form_row">
+                    <label class="contact"><strong>First name:</strong></label>
+                    <input type="text" class="contact_input txtCus_firtName" name="txtCus_firtName" id="txtCus_firtName"/>
+                </div>
+                <div class="form_row">
+                    <label class="contact"><strong>Last name:</strong></label>
+                    <input type="text" class="contact_input txtCus_lastName" name="txtCus_lastName" id="txtCus_lastName"/>
+                </div>
+                <div class="form_row">
+                    <label class="contact"><strong>Email:</strong></label>
+                    <input type="text" class="contact_input txtCus_email" name="txtCus_email" id="txtCus_email"/>
+                </div>
+                <div class="form_row">
+                    <label class="contact"><strong>Phone:</strong></label>
+                    <input type="text" class="contact_input txtCus_phone" name="txtCus_phone" id="txtCus_phone"/>
+                </div>
+                <div class="form_row">
+                    <label class="contact"><strong>Adrres:</strong></label>
+                    <input type="text" class="contact_input txtCus_adrres" name="txtCus_adrres" id="txtCus_adrres"/>
+                </div>
             </div>
             <div class="form_row">
                 <label class="contact"><strong>Requirement:</strong></label>
@@ -40,7 +46,7 @@
             </div>
             <div class="form_row">
                 <label class="contact"><strong>Delivery date: </strong></label>
-                <input type="text" class="contact_input" name="txtDelivery_date"/>
+                <input type="datetime" class="contact_input txtDelivery_date datepicker" name="txtDelivery_date" id="txtDelivery_date"/>
             </div>
             <br><br>
             <p>
