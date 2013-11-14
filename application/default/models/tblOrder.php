c8f76b489c8191a90ed7b779dff9ffd603b6ec8b
ThaiNV <thainv@smartosc.com>
2013-11-15 02:14:51 +0700

    ThaiNV edit cart for finishing

--- application/default/models/tblOrder.php
+++ application/default/models/tblOrder.php
@@ -17,18 +17,36 @@ class Default_Models_tblOrder extends Models_tblOrder {
      */
     public function saveOrder($aryReceived, $payId) {
         //reBuild data
+        $lastId = '';
+
         $aryDataInsert = array(
             'cus_id' => 1,
             'pay_id' => $payId,
             'order_date' => date('Y-m-d H:i:s'),
-//            'delivery_date' => date('Y-m-d H:i:s'),
-            'requirement' => 1,
+            'delivery_date' => date('Y-m-d H:i:s', strtotime($aryReceived['date'])),
+            'requirement' => $aryReceived['requirement'],
             'name_recipient' => $aryReceived['name'],
             'address_recipient' => $aryReceived['adress'],
             'email_recipient' => $aryReceived['email'],
             'phone_recipient' => $aryReceived['phone'],
         );
-        $this->_queryUnit->getInsert('tbl_orders', $aryDataInsert);
+        $this->_queryUnit->getInsert('tbl_orders', $aryDataInsert, $lastId);
+        return $lastId;
+    }
+
+    /**
+     * @desc check existing of customer
+     * 
+     * @author ThaiNV
+     * @since 14/11/2013
+     */
+    public function checkExistCustom($email) {
+        if (isset($email)) {
+            $condition = 'email = ' . $email;
+            $res = $this->_queryUnit->getSelect('tbl_customers', $condition);
+            $cusInfo = $this->_queryUnit->fetchOne($res);
+        }
+        return $cusInfo;
     }
 
 }
